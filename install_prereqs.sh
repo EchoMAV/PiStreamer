d5a89125bbc81c7868cf312b83f7ad3ea6e3178b
Brad Stinson <bradstinson@gmail.com>
2024-09-10 20:06:07 -0400

    Create install_prereqs.sh

---
+++ install_prereqs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# install prereqs for PiStreamer and associated tools
+
+SUDO=$(test ${EUID} -ne 0 && which sudo)
+$SUDO apt update
+$SUDO apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+$SUDO apt install -y python3-libcamera libcamera-apps
+$SUDO apt install -y python3-picamera2
+$SUDO apt install -y ffmpeg
