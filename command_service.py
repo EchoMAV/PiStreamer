b3c5e0a3596154b491e6b2b284e92eb5bc398345
Benjamin Castor <bcastor@echomav.com>
2024-09-28 21:02:30 -0400

    comments

---
+++ command_service.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+from typing import List, Optional, Tuple
+from constants import INPUT_FIFO_PATH, OUTPUT_FIFO_PATH, CommandType
+import os
+
+"""
+Commands are communicated to the pistreamer app via a dedicated FIFO queue and status
+is returned back to the mavlink service with a different FIFO queue.
+"""
+
+class CommandService:
+    def __init__(self):
+        if not os.path.exists(INPUT_FIFO_PATH):
+            os.mkfifo(INPUT_FIFO_PATH)
+        if not os.path.exists(OUTPUT_FIFO_PATH):
+            os.mkfifo(OUTPUT_FIFO_PATH)
+
+    def add_input_command(self, command_type: CommandType, command_value: Optional[str] = "") -> None:
+        """
+        This is a way to write to the input FIFO queue.
+        """
+        try:
+            fifo_fd = os.open(INPUT_FIFO_PATH, os.O_WRONLY | os.O_NONBLOCK)
+            os.write(fifo_fd, f"{command_type.value} {command_value}\n".encode())
+            os.close(fifo_fd)
+        except Exception as e:
+            print(f"Error writing to FIFO: {e}")
+            raise e
+
+    def add_output_data(self, data:str) -> None:
+        """
+        This is a way to write to the output FIFO queue.
+        """
+        try:
+            fifo_fd = os.open(OUTPUT_FIFO_PATH, os.O_WRONLY | os.O_NONBLOCK)
+            os.write(fifo_fd, f"{data}\n".encode())
+            os.close(fifo_fd)
+        except Exception as e:
+            print(f"Error writing to FIFO: {e}")
+            raise e
+
+
+    def get_pending_commands(self) -> List[Tuple[str, str]]:
+        """
+        Returns the a list of command that has not been read yet from the FIFO.
+        The first param in the tuple is the command type followed by the command value.
+        If no commands are ready, then an empty list is returned.
+        """
+        commands = []
+        fifo_fd = None
+        try:
+            # Try reading from the FIFO
+            fifo_fd = os.open(INPUT_FIFO_PATH, os.O_RDONLY | os.O_NONBLOCK)
+            data = os.read(fifo_fd, 1024)  # Read up to 1024 bytes
+            if data:
+                data = str(data.decode()).split("\n")
+                data = [item for item in data if item]
+                print(f"Received {len(data)} total command(s): {data}")
+                for command in data:
+                    command_details = command.strip().split(" ")
+                    commands.append((command_details[0].strip(), command_details[1].strip() if len(command_details) > 1 else ""))
+        except Exception as e:
+            print(e)
+        finally:
+            if fifo_fd is not None:
+                os.close(fifo_fd)
+        return commands
---
+++ specs.md
@@ -0,0 +1,49 @@
+# IMX477 EchoLITE SBX Performance Specs
+Below are bench tested results of the IMX477 functioning at various resolutions
+and capture modes. Captured video and photo files are saved to the RPi filesystem whilst
+the streaming destination a RTP feed to a configuration IP and port.
+
+## Resolution & Aspect Ratio
+| Resolution | Aspect Ratio | Notes         |
+|------------|--------------|---------------|
+| 640x360    | 16:9         | 360p LD       |
+| 854x480    | 16:9         | 480p SD       |
+| 1280x720   | 16:9         | 720p HD       |
+| 1920x1080  | 16:9         | 1080p Full HD |
+| 3840x2160  | 16:9         | 4K            |
+| 4056x3040  | 4:3          | 12 MP         |
+
+## Streaming Only (2M bitrate)
+| Resolution | Avg. Frame Rate | Aspect Ratio | Notes         |
+|------------|-----------------|--------------|---------------|
+| 640x360    | 50              | 16:9         | 360p LD       |
+| 854x480    | 50              | 16:9         | 480p SD       |
+| 1280x720   | 45              | 16:9         | 720p HD       |
+| 1920x1080  | 20              | 16:9         | 1080p Full HD |
+
+## Streaming + Saved Video (2M bitrate)
+| Streaming Resolution | Video Resolution | Avg. Frame Rate |
+|----------------------|------------------|-----------------|
+| 640x360              | 640x360          | 48              |
+| 854x480              | 854x480          | 47              |
+| 1280x720             | 1280x720         | 34              |
+| 1920x1080            | 1920x1080        | 13              |
+| 1280x720             | 3840x2160        | N/A             |
+
+## Streaming + Saved Photo (2M bitrate)
+In order to take a photo at higher resolution than streaming resolution, there is a
+momentary delay in the camera software to switch between resolutions. Capturing photos
+at the same resolution as streaming incurs no significant delay.
+| Streaming Resolution | Photo Resolution | Avg. Photo Delay |
+|----------------------|------------------|------------------|
+| Not 1920x1080        | 1920x1080        | .75 sec          |
+| Not 3840x2160        | 3840x2160        | 1.15 sec         |
+| Not 4056x3040        | 4056x3040        | 1.42 sec         |
+
+
+## Streaming + Saved Video + Saved Photo (2M bitrate)
+To save a photo at the same resolution as streaming and recording video, there is no
+significant impact to fps or streaming delay. However, if capturing a photo at a
+higher resolution than streaming+video, the photo delay time above will apply and the
+mp4 file up to that point will be saved and a new mp4 file will resume after the photo
+has been captured.
