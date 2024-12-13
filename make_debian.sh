99b69bcde6937365a90b00b708b9eeecbccc591d
Benjamin Castor <bcastor@echomav.com>
2024-12-13 12:25:35 -0500

    handle copying over

--- make_debian.sh
+++ make_debian.sh
@@ -8,6 +8,8 @@ SUDO=$(test ${EUID} -ne 0 && which sudo)
 $SUDO apt install -y dpkg
 
 cd "pistreamer"
+$SUDO cp usr/lib/python3.11/dist-packages/pistreamer/pistreamer.py usr/bin/pistreamer
+$SUDO chmod 755 usr/bin/pistreamer
 dpkg-deb --root-owner-group --build . ../
 cd ..
 $SUDO apt install ./pistreamer_* --reinstall
