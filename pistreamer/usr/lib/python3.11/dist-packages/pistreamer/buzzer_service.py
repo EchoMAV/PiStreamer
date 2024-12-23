abc5b3c9bcb36d0d4971ffb3a55b47e61052b490
Benjamin Castor <bcastor@echomav.com>
2024-12-23 16:15:35 -0500

    switch gpio signals

--- pistreamer/usr/lib/python3.11/dist-packages/pistreamer/buzzer_service.py
+++ pistreamer/usr/lib/python3.11/dist-packages/pistreamer/buzzer_service.py
@@ -1,3 +1,4 @@
+from typing import Final
 import RPi.GPIO as GPIO
 import time
 
@@ -5,15 +6,14 @@ BUZZER_PIN = 6
 ON_DURATION = 0.08
 SLOW_HEART_BEEP_DURATION = 1.95
 FAST_HEART_BEEP_DURATION = 1.35
+GPIO_HIGH: Final = 0  # the SBX board inverts this logic
+GPIO_LOW: Final = 1  # the SBX board inverts this logic
 """
 # Buzzer Specs
 ## Pairing Drone
 Single beep slow heartbeat (Drone is in pairing mode scanning for QR codes)
 Three quick beeps (Drone has successfully scanned the QR code)
 Double beep slow heartbeat (Pairing is in progress)
-## SD Card Operations
-Single quick beep (SD card detected)
-Double beep fast heartbeat (Software update in progress)
 """
 
 
@@ -24,12 +24,12 @@ class BuzzerService:
 
     def quick_beep(self):
         try:
-            GPIO.output(BUZZER_PIN, GPIO.HIGH)
+            GPIO.output(BUZZER_PIN, GPIO_HIGH)
             time.sleep(ON_DURATION)
-            GPIO.output(BUZZER_PIN, GPIO.LOW)
+            GPIO.output(BUZZER_PIN, GPIO_LOW)
             time.sleep(0.065)
         except Exception:
-            GPIO.output(BUZZER_PIN, GPIO.LOW)
+            GPIO.output(BUZZER_PIN, GPIO_LOW)
 
     def double_beep_slow_heartbeat(self):
         try:
@@ -38,7 +38,7 @@ class BuzzerService:
                 self.quick_beep()
                 time.sleep(SLOW_HEART_BEEP_DURATION)
         except Exception:
-            GPIO.output(BUZZER_PIN, GPIO.LOW)
+            GPIO.output(BUZZER_PIN, GPIO_LOW)
 
     def single_beep_slow_heartbeat(self):
         try:
@@ -46,11 +46,11 @@ class BuzzerService:
                 self.quick_beep()
                 time.sleep(SLOW_HEART_BEEP_DURATION)
         except Exception:
-            GPIO.output(BUZZER_PIN, GPIO.LOW)
+            GPIO.output(BUZZER_PIN, GPIO_LOW)
 
     def three_quick_beeps(self):
         try:
             for _ in range(0, 3):
                 self.quick_beep()
         except Exception:
-            GPIO.output(BUZZER_PIN, GPIO.LOW)
+            GPIO.output(BUZZER_PIN, GPIO_LOW)
